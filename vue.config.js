1cae962aeef45b2915780d72c055b82382dcc80b
Lukas Bachlechner <lukasbachlechner@hotmail.de>
2020-08-01 13:32:47 +0200

    env

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const PREVIEW_TOKEN = 'MhMUm48rJbU143xQOg1EMwtt';
-const PUBLIC_TOKEN = '';
+process.env.PREVIEW_TOKEN = 'MhMUm48rJbU143xQOg1EMwtt';
+process.env.PUBLIC_TOKEN = '';
 
 process.env.VUE_APP_ACCESS_TOKEN = process.env.STORYBLOK_BRIDGE
-    ? PREVIEW_TOKEN
-    : PUBLIC_TOKEN;
+    ? process.env.PREVIEW_TOKEN
+    : process.env.PUBLIC_TOKEN;
 process.env.VUE_APP_STORYBLOK_BRIDGE = process.env.STORYBLOK_BRIDGE;
 
 module.exports = {};
